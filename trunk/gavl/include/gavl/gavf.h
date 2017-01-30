2bbbac8488129a19f44cf2d79b52e9b0b4827dd6
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2017-01-30 22:55:52 +0000

    * Update

--- trunk/gavl/include/gavl/gavf.h
+++ trunk/gavl/include/gavl/gavf.h
@@ -628,37 +628,4 @@ int gavf_write_gavl_packet(gavf_io_t * io,
                            int64_t last_sync_pts,
                            const gavl_packet_t * p);
 
-/* (de)serialize A/V frames */
-
-GAVL_PUBLIC
-gavl_audio_source_t * gavl_audio_source_create_io(gavf_io_t * io,
-                                                  const gavl_audio_format_t * fmt,
-                                                  const gavl_dictionary_t * m);
-
-GAVL_PUBLIC
-gavl_audio_sink_t * gavl_audio_sink_create_io(gavf_io_t * io,
-                                              gavl_audio_format_t * fmt,
-                                              gavl_dictionary_t * m);
-
-GAVL_PUBLIC
-gavl_video_source_t * gavl_video_source_create_io(gavf_io_t * io,
-                                                  const gavl_video_format_t * fmt,
-                                                  const gavl_dictionary_t * m);
-
-GAVL_PUBLIC
-gavl_video_sink_t * gavl_video_sink_create_io(gavf_io_t * io,
-                                              gavl_video_format_t * fmt,
-                                              gavl_dictionary_t * m);
-
-GAVL_PUBLIC
-gavl_packet_source_t * gavl_packet_source_create_io(gavf_io_t * io,
-                                                    int default_duration,
-                                                    int packet_flags);
-
-GAVL_PUBLIC
-gavl_packet_sink_t * gavl_packet_sink_create_io(gavf_io_t * io,
-                                                int default_duration,
-                                                int packet_flags);
-
-
 #endif // GAVF_H_INCLUDED
