d37f150249523482edfbbaebcccc182c6624e348
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2017-09-24 21:59:00 +0000

    * Convert multipart movies to EDLs

--- include/gavl/edl.h
+++ include/gavl/edl.h
@@ -44,33 +44,7 @@ extern "C" {
 /** \brief One segment of a physical stream to appear in a logical stream
  */
 
-#if 0  
-
-typedef struct gavl_edl_s gavl_edl_t;
-
-typedef struct
-  {
-  char * url;   //!< Location of that segment. If NULL, the "master url" in \ref gavl_edl_t is valid.
-
-  int track;        //!<  Track index for multitrack inputs
-  int stream;       //!<  Index of the A/V stream
-  int timescale;    //!<  Source timescale
-    
-  int64_t src_time; //!< Time within the source in source timescale
-  
-  /* Time and duration within the destination in destination
-     timescale */
-  int64_t dst_time;  //!< Time  within the destination in destination timescale
-  int64_t dst_duration; //!< Duration within the destination in destination timescale
-
-  /*  */
-  int32_t speed_num; //!< Playback speed numerator
-  int32_t speed_den; //!< Playback speed demoninator
-  
-  } gavl_edl_segment_t;
-#else
 typedef gavl_dictionary_t gavl_edl_segment_t;
-#endif
 
 GAVL_PUBLIC 
 void gavl_edl_segment_set_url(gavl_edl_segment_t * seg, const char * url); 
@@ -105,107 +79,6 @@ int gavl_edl_segment_get(const gavl_edl_segment_t * seg,
 
 GAVL_PUBLIC
 void gavl_edl_finalize(gavl_dictionary_t * edl);
-  
-  
-/** \brief A locical stream
- */
-#if 0
-typedef struct
-  {
-  gavl_edl_segment_t * segments; //!< Segments
-  int num_segments;              //!< Number of segments 
-  int timescale;                 //!< Destination timescale
-  } gavl_edl_stream_t;
-
-/** \brief A locical track
- */
-
-typedef struct
-  {
-  gavl_dictionary_t   metadata;        //!< Metadata (optional)
-  
-  int num_audio_streams;             //!< Number of logical audio streams
-  gavl_edl_stream_t * audio_streams; //!< Logical audio streams
-
-  int num_video_streams;             //!< Number of logical video streams
-  gavl_edl_stream_t * video_streams; //!< Logical video streams
-
-  int num_text_streams;     //!< Number of logical text subtitle streams
-  gavl_edl_stream_t * text_streams; //!< Logical text subtitle streams
-
-  int num_overlay_streams;  //!< Number of logical overlay subtitle streams
-  gavl_edl_stream_t * overlay_streams; //!< Logical overlay subtitle streams
-  
-  } gavl_edl_track_t;
-
-/** \brief EDL structure
- */
-
-struct gavl_edl_s
-  {
-  int num_tracks;             //!< Number of logical tracks
-  gavl_edl_track_t * tracks;  //!< Logical tracks
-  char * url;                 //!< Filename if all streams are from the same file
-  };
- 
-
-/** \brief Append a track to the EDL
-    \param e An EDL
- *  \returns The new track
- */
-
-GAVL_PUBLIC
-gavl_edl_track_t * gavl_edl_add_track(gavl_edl_t * e);
-
-/** \brief Append an audio stream to an EDL track
-    \param t An EDL track
- *  \returns The new stream
- */
-
-GAVL_PUBLIC
-gavl_edl_stream_t * gavl_edl_add_audio_stream(gavl_edl_track_t * t);
-
-/** \brief Append a video stream to an EDL track
-    \param t An EDL track
- *  \returns The new stream
- */
-
-GAVL_PUBLIC
-gavl_edl_stream_t * gavl_edl_add_video_stream(gavl_edl_track_t * t);
-
-/** \brief Append a text stream to an EDL track
-    \param t An EDL track
- *  \returns The new stream
- */
-
-GAVL_PUBLIC
-gavl_edl_stream_t * gavl_edl_add_text_stream(gavl_edl_track_t * t);
-
-/** \brief Append an overlay stream to an EDL track
-    \param t An EDL track
- *  \returns The new stream
- */
-
-GAVL_PUBLIC
-gavl_edl_stream_t * gavl_edl_add_overlay_stream(gavl_edl_track_t * t);
-
-/** \brief Copy an entire EDL
-    \param e An EDL
- *  \returns Copy of the EDL
- */
-
-GAVL_PUBLIC
-gavl_edl_t * gavl_edl_copy(const gavl_edl_t * e);
-
-/** \brief Destroy an EDL and free all memory
-    \param e An EDL
- */
-
-GAVL_PUBLIC
-void gavl_edl_destroy(gavl_edl_t * e);
-
-  
-#endif
 
 /** \brief Create an empty EDL
  *  \returns A newly allocated EDL
@@ -223,6 +96,11 @@ gavl_dictionary_t * gavl_edl_create(gavl_dictionary_t * parent);
 GAVL_PUBLIC
 gavl_edl_segment_t * gavl_edl_add_segment(gavl_dictionary_t * s);
 
+GAVL_PUBLIC
+void gavl_edl_append_track_to_timeline(gavl_dictionary_t * edl_track,
+                                    const gavl_dictionary_t * track, int init);
+
+  
 #if 0
 
 /** \brief Dump an EDL to stderr
