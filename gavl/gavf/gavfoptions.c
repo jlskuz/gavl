733dda99c3886ff13676d7532ee074630364ceff
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2019-09-18 22:44:58 +0000

    * Update

--- gavl/gavf/gavfoptions.c
+++ gavl/gavf/gavfoptions.c
@@ -1,5 +1,6 @@
 #include <gavfprivate.h>
 #include <string.h>
+#include <stdlib.h>
 
 void
 gavf_options_set_sync_distance(gavf_options_t * opt, gavl_time_t sync_distance)
@@ -23,3 +24,13 @@ void gavf_options_copy(gavf_options_t * dst, const gavf_options_t * src)
   memcpy(dst, src, sizeof(*src));
   }
 
+gavf_options_t * gavf_options_create()
+  {
+  gavf_options_t * ret = calloc(1, sizeof(*ret));
+  return ret;
+  }
+
+void gavf_options_destroy(gavf_options_t * opt)
+  {
+  free(opt);
+  }
