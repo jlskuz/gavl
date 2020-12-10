2c963433016977793abdc54fdd2cab6650772bfc
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2020-12-10 21:55:45 +0000

    * Update

--- gavl/hw_v4l2.c
+++ gavl/hw_v4l2.c
@@ -954,18 +954,18 @@ int gavl_v4l_device_init_decoder(gavl_v4l_device_t * dev, gavl_dictionary_t * st
     goto fail;
     }
   
-  /* Create buffers */
-
-  if(!(dev->num_in_bufs = request_buffers_mmap(dev, buf_type, DECODER_NUM_FRAMES, dev->in_bufs)))
-    goto fail;
-  
   if(!stream_on(dev, buf_type))
     goto fail;
 
   /* Wait for source_change event */
   do_poll(dev, &can_read, &can_write, &has_event);
   fprintf(stderr, "do_poll (init) %d %d %d\n", can_read, can_write, has_event);
+
+  /* Create buffers */
   
+  if(!(dev->num_in_bufs = request_buffers_mmap(dev, buf_type, DECODER_NUM_FRAMES, dev->in_bufs)))
+    goto fail;
+
   
   //  handle_decoder_event(dev);
   
