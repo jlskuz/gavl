e2759c8fec7869d189e634a39cd840d5ec2315fd
gmerlin <gmerlin@214d8ac5-8513-4f59-80f8-2f39b674a24a>
2020-12-18 22:06:17 +0000

    * Update

--- trunk/gavl/include/gavl/hw_v4l2.h
+++ trunk/gavl/include/gavl/hw_v4l2.h
@@ -64,24 +64,11 @@ GAVL_PUBLIC gavl_packet_source_t * gavl_v4l_device_get_packet_source(gavl_v4l_de
 GAVL_PUBLIC gavl_video_sink_t * gavl_v4l_device_get_video_sink(gavl_v4l_device_t * dev);
 GAVL_PUBLIC gavl_video_source_t * gavl_v4l_device_get_video_source(gavl_v4l_device_t * dev);
 
-#if 0
-GAVL_PUBLIC int gavl_v4l_device_init_capture(gavl_v4l_device_t * dev,
-                                             gavl_video_format_t * fmt);
-
-GAVL_PUBLIC int gavl_v4l_device_init_output(gavl_v4l_device_t * dev,
-                                            gavl_video_format_t * fmt);
-
-GAVL_PUBLIC int gavl_v4l_device_init_encoder(gavl_v4l_device_t * dev,
-                                             gavl_video_format_t * fmt,
-                                             gavl_compression_info_t * cmp);
-
-GAVL_PUBLIC int gavl_v4l_device_init_decoder(gavl_v4l_device_t * dev,
-                                             gavl_video_format_t * fmt,
-                                             const gavl_compression_info_t * cmp);
-#endif
 
 GAVL_PUBLIC int gavl_v4l_device_init_decoder(gavl_v4l_device_t * dev, gavl_dictionary_t * stream,
                                              gavl_packet_source_t * psrc);
+
+GAVL_PUBLIC void gavl_v4l_device_resync_decoder(gavl_v4l_device_t * dev);
 
 GAVL_PUBLIC void gavl_v4l_device_info(const char * dev);
 GAVL_PUBLIC void gavl_v4l_device_infos();
